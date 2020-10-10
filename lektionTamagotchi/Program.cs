4d801dbb844fef6e526d3ad32168528965280f07
Linnea <linnea@klangby.com>
2020-10-10 11:33:49 +0200

    jobbat med klassen och påbörjat jobb i program.cs

--- lektionTamagotchi/Program.cs
+++ lektionTamagotchi/Program.cs
@@ -7,7 +7,17 @@ namespace lektionTamagotchi
         static void Main(string[] args)
         {
             Tamagotchi t1 = new Tamagotchi();
-            
+            t1.name = Name();
+            Console.ReadLine();
+        }
+        static string Name()
+        {
+            string name = Console.ReadLine();
+            while (name.Length > 10 && name.Length < 1)
+            {
+                Console.WriteLine("Skriv ett giltigt namn mellan 1-10 bokstäver.");
+            }
+                return name;
         }
     }
 }
--- lektionTamagotchi/Tamagotchi.cs
+++ lektionTamagotchi/Tamagotchi.cs
@@ -12,9 +12,51 @@ namespace lektionTamagotchi
         private bool isAlive = true;
         public string name = "";
 
-        public void Feed(int hunger)
+        public void Feed()
         {
-            hunger -= 1;
+            this.hunger -= 1;
+        }
+        private void ReduceBoredome()
+        {
+            this.boredom -= 1;
+        }
+        public void Hi()
+        {
+            int number = generator.Next(this.words.Count - 1);
+            Console.WriteLine(this.words[number]);
+            ReduceBoredome();
+        }
+        public void Teach(string word)
+        {
+            word = Console.ReadLine();
+            this.words.Add(word);
+            ReduceBoredome();
+        }
+        public void Tick()
+        {
+            this.hunger += 1;
+            this.boredom += 1;
+            if (this.hunger > 10|| this.boredom > 10)
+            {
+                this.isAlive = false;
+            }
+        }
+        public void PrintStats()
+        {
+            Console.WriteLine(name + " har " + this.hunger + " hunger.");
+            Console.WriteLine(name + " är " + this.boredom + "uttråkad.");
+            if (GetAlive() == true)
+            {
+                Console.WriteLine(name + " lever.");
+            }
+            else
+            {
+                Console.WriteLine(name + " är död.");
+            }
+        }
+        public bool GetAlive()
+        {
+            return(this.isAlive);
         }
     }
 }
